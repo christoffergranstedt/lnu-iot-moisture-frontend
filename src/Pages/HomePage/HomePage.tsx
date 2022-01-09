4d846e36aa932867d39e8ca5a8d6a3bcbe5a5e13
ChristofferGranstedt <christoffer.granstedt@outlook.com>
2022-01-09 14:06:26 +0100

    setting homepage on smallest size

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -18,34 +18,34 @@ export const HomePage: React.FC<HomePageProps> = (props) => {
 	
 	return (
 		<div className="h-full">
-			<div className="mx-auto flex flex-wrap place-content-center text-center items-center py-16 bg-gradient-to-t from-primary to-primary-hover">
+			<div className="mx-auto flex flex-wrap place-content-center text-center items-center py-32 bg-gradient-to-t from-primary to-primary-hover">
 				<div className="container">
 					<h1 className="text-7xl pb-12 w-full"><span className="text-3xl md:text-9xl text-secondary">Granstedt's</span><br/> Smart Things Hub</h1>
 				</div>
 			</div>
-			<div className="bg-sixth py-8">
+			<div className="bg-sixth py-32">
 				<div className="container mx-auto flex flex-wrap ">
 					<div className="flex-1 self-center text-center">
-						<h3 className="text-3xl inline-block mb-8">Invoke actions, subscribe to events and see property values for your things</h3>
+						<h3 className="text-2xl sm:text-3xl inline-block mb-4 sm:mb-8 p-2">Invoke actions, subscribe to events and see property values for your things</h3>
 						{!isSignedIn() && <button className="bg-none border-2 border-secondary px-8 py-2 rounded-lg mt-12 transform transition duration-500 hover:scale-110 hover:bg-secondary hover:text-gray-800 text-xl" onClick={() => navigate({ pathname: '/auth/signin' })}>sign in</button>}
 					</div>
-					<div className="flex-1">
-						<PlantImage className="mx-auto m-8 mb-16" style={{ width: 400, height: 400}}/>
+					<div className="flex-1 mt-8 sm:mt-0">
+						<PlantImage className="mx-auto m-8 mb-16 w-[300px] h-[300px]"/>
 					</div>
 				</div>
 
 			</div>
-			<div className="bg-fourth py-20 flex flex-wrap justify-center text-gray-800">
-				<InfoBox className="m-4">
-					<InternetOfThingsImage className="mx-auto m-2" style={{ width: 250, height: 250}}/>
+			<div className="bg-fourth py-32 flex flex-wrap justify-center text-gray-800">
+				<InfoBox className="m-4 w-full sm:w-auto">
+					<InternetOfThingsImage className="mx-auto m-2 w-[200px] h-[200px]"/>
 					<h5 className="text-xl">Connect to your things from anywhere</h5>
 				</InfoBox>
-				<InfoBox className="m-4">
-					<TelegramImage className="mx-auto m-2" style={{ width: 250, height: 250}}/>
+				<InfoBox className="m-4 w-full sm:w-auto">
+					<TelegramImage className="mx-auto m-2 w-[200px] h-[200px]"/>
 					<h5 className="text-xl">Receive notifications on Telegram when events related to your things occurs</h5>
 				</InfoBox>
-				<InfoBox className="m-4">
-					<WateringImage className="mx-auto m-2" style={{ width: 250, height: 250}}/>
+				<InfoBox className="m-4 w-full sm:w-auto">
+					<WateringImage className="mx-auto m-2 w-[200px] h-[200px]"/>
 					<h5 className="text-xl">Monitor your home</h5>
 				</InfoBox>
 			</div>
